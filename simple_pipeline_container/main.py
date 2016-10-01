ccd99b536a7cc299ae1b861ea450737c2bd8d901
Dzmitry Pamakha <dzmitry.pamakha@purplescout.se>
2016-10-01 18:54:28 +0200

    Implemented simple pipeline container

--- simple_pipeline_container/main.py
+++ simple_pipeline_container/main.py
@@ -1,31 +1,29 @@
-import strictyaml
 import os
 import shutil
 
-SEED_FILE="/app/seed.yml"
+TASK_NAME = 'java-build'
 
-def run_pipeline(name, app_dir, out_dir):
+
+def run_task(name, app_dir, out_dir):
     os.system('docker run --rm -v {app_dir}:/app -e "APP_DIR={app_dir}" '\
               '-v /var/run/docker.sock:/var/run/docker.sock -e "PARENT_OUT_DIR={out_dir}" {name}'
               .format(app_dir = app_dir,
                       out_dir = out_dir,
                       name = name))
 
-def main():
-    with open(SEED_FILE, 'r') as file:
-        seed = strictyaml.load(file.read())
 
+def main():
     app_dir = os.environ['APP_DIR']
     parent_out_dir = os.environ.get('PARENT_OUT_DIR', '')
-    out_dir = parent_out_dir + os.path.sep + 'ci'
+    out_dir = parent_out_dir + os.path.sep + 'simple'
 
     shutil.rmtree('/app/' + out_dir, ignore_errors=True)
     os.mkdir('/app/' + out_dir)
 
-    run_pipeline(seed['pipeline'], app_dir, out_dir)
+    run_task('java-build', app_dir, out_dir)
 
-    open('/app/' + out_dir + '/seed.out', 'w+')
-    print 'Seed finished'
+    open('/app/' + out_dir + '/simple.out', 'w+')
+    print 'Simple finished'
 
 if __name__ == '__main__':
     main()
