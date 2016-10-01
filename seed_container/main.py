7867e9a9fce09657e91f4132f6bfa20386b54260
Dzmitry Pamakha <dzmitry.pamakha@purplescout.se>
2016-10-01 17:39:18 +0200

    Finished seed container main script

--- seed_container/main.py
+++ seed_container/main.py
@@ -1,6 +1,16 @@
-def main():
-    print 'Hello world!'
+import strictyaml
+import os
 
+SEED_FILE="/app/seed.yml"
+
+def run_pipeline(name):
+    os.system("docker run --rm -v /app:/app -v /var/run/docker.sock:/var/run/docker.sock {}".format(name))
+
+def main():
+    with open(SEED_FILE, 'r') as file:
+        seed = strictyaml.load(file.read())
+    run_pipeline(seed['pipeline'])
+    print 'Finish'
 
 if __name__ == '__main__':
     main()
